5cac59583101ad723a2e885c46be03dbf9683edb
Tony Beveridge <tony.beveridge@outlook.co.nz>
2015-11-25 21:02:25 +1300

    Pro forma quorum aware windows service
    Very much a WIP

---
+++ QuorumWindowsService/QuorumAwareWindowsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Quorum.Integration.Http;
+using Infra;
+
+namespace QuorumWindowsService {
+
+    public partial class QuorumAwareWindowsService : ServiceBase {
+
+        private HttpQuorumImpl Adapter { get; set; }
+
+        public QuorumAwareWindowsService() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+            Adapter = new HttpQuorumImpl().Start<WorkerAdapter>();
+        }
+
+        protected override void OnStop() {
+            if (Adapter.IsNotNull())
+                Adapter.Stop().Wait();
+        }
+    }
+}
+
---
+++ QuorumWindowsService/WorkerAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quorum;
+using System.Threading;
+using Infra;
+
+namespace QuorumWindowsService {
+
+    // Replace the implementation of this class with whatever implementation you wish
+
+    public class WorkerAdapter : BaseMasterWorkAdapter {
+
+        protected override async Task<bool> Work() {
+            await Task.Delay(10);
+            return false;
+        }
+
+        protected override async Task Stopping() {
+            await Task.Delay(10);
+        }
+
+        protected override void Stopped() {
+        }
+
+    }
+}
+
